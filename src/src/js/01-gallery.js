464ac88a7e4efbd7e00a0e6285733ee7175ec6e6
EneAndrei789 <andreileonardene@gmail.com>
2024-11-19 22:40:55 +0200

    commit

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `
+  )
+  .join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();  // previne redir.   
+
+  const isImage = event.target.classList.contains('gallery__image');
+  if (!isImage) return;
+
+  const largeImageURL = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  // Închiderea modală la apăsarea tastei Escape
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
--- src/src/js/02-lightbox.js
+++ src/src/js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Selectăm containerul galeriei
+const galleryContainer = document.querySelector('.gallery');
+
+// Generăm marcajul pentru galerie
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `
+  )
+  .join('');
+
+// Adăugăm marcajul în DOM
+galleryContainer.innerHTML = galleryMarkup;
+
+// Inițializăm SimpleLightbox
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', // Textul alternativ va fi afișat ca subtitlu
+  captionDelay: 250,   // Subtitlul apare după 250 ms
+});
+
